b4fe8bddb8ae463764fc2e8f4f9af8c279b5963d
1k <bug_zero@163.com>
2024-08-05 14:45:51 +0800

    fix: When using "&", the "label" class name will be skipped

--- packages/class-namespace/src/index.ts
+++ packages/class-namespace/src/index.ts
@@ -1,7 +1,7 @@
 import type stylis from 'stylis'
 
 export interface optionsType {
-    namespace?: string
+    namespace: string
 }
 export default function classNamespace(options: optionsType): stylis.Middleware {
     return (context, _, selectors) => {
@@ -12,6 +12,7 @@ export default function classNamespace(options: optionsType): stylis.Middleware 
         const rootClass = Array.isArray(rootSelector.props)
             ? rootSelector.props[0]
             : rootSelector.props
+        if (!/^\..*\x20$/.test(options.namespace)) return
         const namespace = options.namespace.replace(/&/g, rootClass)
         selectors?.forEach(selector => {
             if (Array.isArray(selector.props)) {
